40fb8465a07efd38b7eb3330d7d7139d41f229c4
Tegshig <tegshigzugder@gmail.com>
2019-11-27 15:37:18 +0100

    homepage draft- try /homepage/

--- app/urls.py
+++ app/urls.py
@@ -26,3 +26,25 @@ urlpatterns = [
     path('', ctViews.index, name="index"),
     path('admin/', admin.site.urls),
 ]
+
+# Use include() to add paths from the homepage application 
+from django.urls import include
+from django.urls import path
+
+urlpatterns += [
+    path('homepage/', include('homepage.urls')),
+]
+
+'''
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='homepage/', permanent=True)),
+]
+'''
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ homepage/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+#from catalog.models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = 1
+    num_instances = 2
+    
+    # Available books (status = 'a')
+    num_instances_available = 3
+    
+    # The 'all()' is implied by default.    
+    num_authors = 4
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index_homepage.html', context=context)
