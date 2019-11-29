15139b6388572e5e1ab5c6689705b53593776a3b
Tim Reiprich <tim.reiprich@t-online.de>
2019-11-29 13:12:31 +0100

    cleaned up some files

--- registration/urls.py
+++ registration/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path, include
 
-#from django.contrib.auth import logout 
-from django.conf import settings
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('oauth/', include('social_django.urls', namespace='social')),
-   #path('login/', auth.login, name='login'),
    path('logout/', views.logout, name='logout'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,10 +1,5 @@
-from django.shortcuts import render, redirect
-from django.template import loader
-
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from django.contrib.auth import logout as auth_logout
-
-from django.http import HttpResponse
 
 def index(request):
     return render(request, 'registration/index.html')
