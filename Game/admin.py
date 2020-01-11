53921dcb3abace13b7125ad0bb5d11dcbd6f0375
jose125 <j.gbaco.12@gmail.com>
2020-01-11 10:38:41 +0100

    add Treasure model in admin page

--- Game/admin.py
+++ Game/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from Game.models import Game
+from Game.models import Game, Treasure
 
 admin.site.register(Game)
+admin.site.register(Treasure)
