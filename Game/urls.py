587265eacac7552437ae77c4e89846452528a952
Tim Reiprich <tim.reiprich@t-online.de>
2020-01-12 14:16:55 +0100

    fixed treasure_list error

--- Game/urls.py
+++ Game/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('create/', views.create, name="create"),
     path('view/', views.view, name="view"),
     path('treasure/new/<int:id>/', views.treasure_create, name="treasure_create"),
-    # path('treasure/list', views.treasure_list, name="treasure_list"),
+    path('treasure/list', views.treasure_list, name="treasure_list"),
     url(r'treasure/list/(?P<game_id>[0-9]+)/$', views.treasure_list, name='treasure_list'),
 
 ]
