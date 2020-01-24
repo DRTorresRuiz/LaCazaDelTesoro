e197b8b65b66f0e9e7ca3c9613ea3e4da99df057
Leticia Yepez <leticiayepezchavez@gmail.com>
2020-01-24 13:32:17 +0100

    Creation of the new view Play Game. Some changes  in the model and some updates on the treasure view.

--- Game/urls.py
+++ Game/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url('^details/(?P<game_id>\d+)/$', views.details, name="details"),
     url('^join/(?P<game_id>\d+)/$', views.join, name="join"),
     url('^leave/(?P<game_id>\d+)/$', views.leave, name='leave'),
+    url('^play/(?P<game_id>\d+)/$', views.play, name="play"),
     path('treasure/new/<int:id>/', views.treasure_create, name="treasure_create"),
     path('treasure/list', views.treasure_list, name="treasure_list"),
     url(r'treasure/list/(?P<game_id>[0-9]+)/$', views.treasure_list, name='treasure_list'),
